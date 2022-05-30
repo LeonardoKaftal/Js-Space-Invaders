3c38419fa213a91a941a15a877f6b2d92b9bda7c
Leonardo Kaftal <leonardokaftal1@gmail.com>
2022-05-30 19:17:17 +0200

    Basic structure of the project

--- main.js
+++ main.js
@@ -1,35 +1,23 @@
 const canvas = document.querySelector(".canvas");
 const ctx = canvas.getContext('2d');
-const playerImmage = document.querySelector(".player");
+
+canvas.width = "600";
+canvas.height = "600";
 
  
 const gameController = (()=> {
-    let gameVelocity = 10;
 
-    class Player {
-        constructor(x,y,) {
-            this.x = 0;
-            this.y = 0;
-        }
-    }
+    const background = new Image();
+    background.src = "images/space.png"
 
-    const player = new Player();
 
     function draw() {
-        setInterval(draw,1000 / gameVelocity);
-        drawPlayerAndEnemies();
+        setInterval(draw,1000 / 60);
+        ctx.drawImage(background,0,0)
     }
 
-    function drawPlayerAndEnemies() {
-        ctx.fillStyle = "black";
-        ctx.fillRect(0,0,canvas.width,canvas.height);
-        ctx.drawImage(playerImmage, 0,canvas.height- 5 -15,40,20);
-        ctx.fillStyle = "green";
-        ctx.fillRect(5,0, 10,10)
-    }
+     
+   
 
-
-
-    draw()
-
+    draw();
 })();
